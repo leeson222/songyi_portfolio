ca5a433a4a8ca9b36f487dd1c7ac9cc6a848fe0e
leesongyi <song0478@naver.com>
2024-12-16 20:52:16 +0900

    일단 저장

--- html/js/gsap.js
+++ html/js/gsap.js
@@ -14,15 +14,27 @@ window.onload = function() {
 
   const svg = document.querySelectorAll('.svg')
 
-  gsap.to(svg,{
-    transform: 'translate( 0px, 0px )',
-    duration: 1,
-    scrollTrigger: {
-      trigger: '.visual',
-      start: 'top top',
-      toggleActions: 'play none none reverse'
-    }
-  });
+  $(document).ready(function(){
+    // 페이지 로드 후 비주얼 텍스트 애니메이션 실행
+    
+    gsap.to('body',{
+      opacity: 1,
+      duration: 1.5,
+      onComplete: function () {
+        gsap.to(svg,{
+          transform: 'translate( 0px, 0px )',
+          duration: 1.5,
+          delay: 0.3,
+          onComplete: function () {
+            gsap.to('.visualTxt-small',{
+              opacity: 1,
+              duration: 1.5
+            })
+          }
+        });
+      }
+    })
+  })
 
   gsap.to('.visualCover',{
     opacity: 1,
@@ -92,14 +104,45 @@ window.onload = function() {
     }
   });
 
+  gsap.to('.portfolioBg-bg strong', {
+    scale: 0.5,
+    duration: 1,
+    scrollTrigger: {
+      trigger: '.introduce',
+      start: 'bottom top+=10%',
+      toggleActions: 'play none none reverse'
+    }
+  });
+
   gsap.to('.portfolioWrap', {
-    bottom: '0vh',
+    top: '0',
     duration: 1.5,
     scrollTrigger: {
       trigger: '.introduce',
       start: 'bottom top',
       toggleActions: 'play none none reverse'
     }
+  });
+
+  // 모든 .portfolioBox에 애니메이션 적용
+  gsap.utils.toArray(".portfolioBox").forEach((box) => {
+    gsap.fromTo(
+      box,
+      { opacity: 0, y: 50 }, // 초기 상태
+      {
+          opacity: 1,
+          y: 0,
+          duration: 1,
+          ease: "power2.out",
+          scrollTrigger: {
+              trigger: box,         // 각 박스를 트리거로 사용
+              start: "top =+700",     // 화면에 박스의 top이 80% 지점에 도달하면
+              end: "top =+200",       // 종료 지점 (필요에 따라 설정)
+              toggleActions: "play none none reverse", // 애니메이션 재생
+              markers: true         // 디버깅용 마커 표시
+          }
+      }
+    );
   });
 
 };
